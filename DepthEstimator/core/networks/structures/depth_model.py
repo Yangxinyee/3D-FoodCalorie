496e5149e8323847c0484cc145240a6b796285d1
Zhang Haodong <zhanghaodong@Zhangs-MacBook-Air.local>
2025-04-23 20:16:57 -0400

    Finished the depth model

--- DepthEstimator/core/networks/structures/depth_model.py
+++ DepthEstimator/core/networks/structures/depth_model.py
@@ -1,19 +1,23 @@
 from __future__ import absolute_import, division, print_function
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-import numpy as np
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
-import torch.utils.model_zoo as model_zoo
-from collections import OrderedDict
+from depth_decoder import DepthDecoder
+from depth_encoder import DepthEncoder
 
 
 class Depth_Model(nn.Module):
-    def __init__(self, num_layers=50):
+    def __init__(self, depth_scale, num_layers=50):
         super(Depth_Model, self).__init__()
-        pass
+        self.depth_scale = depth_scale
+        self.encoder = DepthEncoder()
+        self.decoder = DepthDecoder(self.encoder.num_ch_enc, scales=range(depth_scale))
 
     def forward(self, x):
-        pass
+        features = self.encoder(x)
+        outputs = self.decoder(features)
+        disp_list = []
+        for i in range(self.depth_scale):
+            disp = outputs['disp', i]
+            disp_list.append(disp)
+        return disp_list
