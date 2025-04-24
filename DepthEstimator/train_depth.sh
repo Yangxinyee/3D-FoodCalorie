b9e2413df23620901203c3dd1a543ac0e2a86adc
haodong_zhang@brown.edu <hzhan351@node1901.oscar.ccv.brown.edu>
2025-04-23 21:26:42 -0400

    Make batch size become 8

--- DepthEstimator/train_depth.sh
+++ DepthEstimator/train_depth.sh
@@ -38,7 +38,7 @@ python train.py \
   --prepared_save_dir KITTI_prepared \
   --model_dir /users/hzhan351/scratch/checkpoints \
   --flow_pretrained_model /users/hzhan351/scratch/checkpoints/flow/last.pth \
-  --batch_size 16 \
+  --batch_size 8 \
   --num_workers 4 \
   --no_test \
   --lr 0.0001
