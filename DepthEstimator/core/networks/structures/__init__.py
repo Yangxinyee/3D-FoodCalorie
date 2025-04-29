789b9d1224c6660bde8d2420e47093275f1b6ea2
haodongzhang0118 <haodoz0118@gmail.com>
2025-04-28 23:18:31 -0400

    Added old encoder model

--- DepthEstimator/core/networks/structures/__init__.py
+++ DepthEstimator/core/networks/structures/__init__.py
@@ -4,5 +4,6 @@ from feature_pyramid import FeaturePyramid
 from pwc_tf import PWC_tf
 from ransac import reduced_ransac
 from depth_model import Depth_Model
+from depth_model_old import Depth_Model_old
 from net_utils import conv, deconv, warp_flow
 from inverse_warp import inverse_warp2
