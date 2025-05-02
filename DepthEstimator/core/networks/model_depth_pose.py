47d67fd5d1ff0bf7401e7f2be5c3d81ab01d3dc8
Zhang Haodong <zhanghaodong@Zhangs-MacBook-Air.local>
2025-05-02 15:15:14 -0400

    Test code

--- DepthEstimator/core/networks/model_depth_pose.py
+++ DepthEstimator/core/networks/model_depth_pose.py
@@ -19,7 +19,8 @@ class Model_depth_pose(nn.Module):
         self.w_flow_error = cfg.w_flow_error
         self.dataset = cfg.dataset
 
-        self.depth_net = Depth_Model(cfg)
+        # self.depth_net = Depth_Model(cfg)
+        self.depth_net = Depth_Model_old(cfg.depth_scale)
         self.model_pose = Model_triangulate_pose(cfg)
 
     def meshgrid(self, h, w, device=None):
