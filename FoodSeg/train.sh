05ac9f88ce1f7c7a38bfb7f12bf7865dcdd816fb
haodongzhang0118 <haodoz0118@gmail.com>
2025-05-07 16:59:12 +0000

    Fixed the error of train.sh

--- FoodSeg/train.sh
+++ FoodSeg/train.sh
@@ -11,13 +11,13 @@ conda activate food37
 nvidia-smi
 
 # Navigate to your project directory
-cd /home/3D-FoodCalorie/DepthEstimator
+cd /home/3D-FoodCalorie/FoodSeg
 
 # Check PyTorch and CUDA details
 python -c "import torch; print('PyTorch version:', torch.__version__); print('CUDA available:', torch.cuda.is_available()); print('CUDA version:', torch.version.cuda if torch.cuda.is_available() else 'NA'); print('GPU count:', torch.cuda.device_count()); print('GPU name:', torch.cuda.get_device_name(0) if torch.cuda.device_count() > 0 else 'NA')"
 
 # Run training script
-python train.py \
+python finetune.py \
   --gpus 0,1 \
   --batch_size 256 \
   --num_epochs 30 \
