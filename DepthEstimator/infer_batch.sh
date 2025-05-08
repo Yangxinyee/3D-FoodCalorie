c2815c7ee95ffa035b63fde96c95caee696504b9
Zhang Haodong <zhanghaodong@Zhangs-MacBook-Air.local>
2025-05-07 20:32:01 -0400

    Changed the sh file

--- DepthEstimator/infer_batch.sh
+++ DepthEstimator/infer_batch.sh
@@ -11,7 +11,7 @@ conda activate food37
 nvidia-smi
 
 # Navigate to your project directory
-cd /home/3D-FoodCalorie/DepthEstimator
+cd /home/zhd/3D-FoodCalorie/DepthEstimator
 
 # Check PyTorch and CUDA availability
 python -c "import torch; print('PyTorch version:', torch.__version__); print('CUDA available:', torch.cuda.is_available()); print('CUDA version:', torch.version.cuda if torch.cuda.is_available() else 'NA'); print('GPU count:', torch.cuda.device_count()); print('GPU name:', torch.cuda.get_device_name(0) if torch.cuda.device_count() > 0 else 'NA')"
